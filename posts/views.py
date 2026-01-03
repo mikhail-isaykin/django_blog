8b7873144cbf888fd6eab53f3ee13f990926a665
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-01-03 23:37:35 +0300

    Setup templates, header, footer, base; create home and post_list views with URLs

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+def home(request):
+    posts = Post.objects.order_by('-updated_at')[:5]
+    return render(request, 'posts/home.html', {'posts': posts})
+
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/post_list.html', {'posts': posts})
