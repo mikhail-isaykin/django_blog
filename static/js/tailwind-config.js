819daa2c038916600b7d3bf97e330b02d80fb362
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-01-10 23:29:48 +0300

    Rework base.html, bootstrap and tailwind connection

---
+++ static/js/tailwind-config.js
@@ -0,0 +1,10 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+                pastelPink: '#FBCFE8',
+                pastelBlue: '#A5F3FC'
+            }
+        }
+    }
+}
