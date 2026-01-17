1a8daa6bb28356fcfe5b83979a27200fa53a9d34
Mikhail Isaykin <6l2i6.gh@gmail.com>
2026-01-17 23:12:58 +0300

    Minor fix buttons

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,8 +4,6 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path("register/", views.register, name="register"),
-    path(
-        "login/", LoginView.as_view(template_name="accounts/login.html"), name="login"
-    ),
-    path("logout/", LoginView.as_view(next_page="home"), name="logout"),
+    path("login/", LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path("logout/", LogoutView.as_view(next_page="home"), name="logout"),
 ]
